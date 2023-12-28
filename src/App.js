4fe85b7ff4de74b3c882600dff3fd18062f8a23a
Pranjal <tataisarkar2001@gmail.com>
2023-12-28 21:05:18 +0530

    Frontend | Made Footer for the website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './Components/Navbar/Navbar.tsx';
+import Footer from './Components/Footer/Footer.tsx';
 
 function App() {
   return (
     <>
       <ResponsiveAppBar />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+        <div className="footer-container">
+            <footer>
+                Made With &#10084; by Tatai Sarkar
+            </footer>
+        </div>
+    </>
+  )
+}
+
+export default Footer
